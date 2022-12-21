065e8bf75a64762c5b08499b04eeec0461445d1a
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-21 18:43:08 +0530

    Added my first model of the UserSchema

---
+++ server/models/User.model.js
@@ -0,0 +1,48 @@
+const { model, Schema, default: mongoose } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 2,
+      max: 100,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      max: 100,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      min: 2,
+    },
+    city: String,
+    state: String,
+    country: String,
+    occupation: String,
+    phoneNumber: String,
+    transactions: Array,
+    role: {
+      type: String,
+      enum: ["user", "admin", "superadmin"],
+      default: "admin",
+    },
+  },
+  { timestamps: true }
+);
+
+/* 
+ In Mongo DB, the collection name will be "users" and it will have the same structure as mentioned in the userSchema, we can use "users" collection by using the "User"
+*/
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- server/routes/general.route.js
+++ server/routes/general.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+import { getUser } from "../controllers/general.controller";
+
 const router = express.Router();
+
+router.get("/user/:id", getUser);
 
 module.exports = router;
