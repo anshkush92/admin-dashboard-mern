11aae4b13e5a560875eb49567ba8cf5b0c8b760c
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-21 21:50:21 +0530

    Now we have set up our redux toolkit with the react query, now we are good to go

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,13 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 import toggleMode from "../features/Toggle/ToggleMode.slice";
 import toggleSidebar from "../features/Toggle/ToggleSidebar.slice";
+import { api } from "../services/api/api";
 
 const store = configureStore({
   reducer: {
     toggleMode,
     toggleSidebar,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export default store;
