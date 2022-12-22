3e852af599706e3610fed7452061b7cb8fef03a7
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-22 23:56:27 +0530

    Added the data int he format whcih is needed bhy the nivo line chart to show us the line chart correctly

--- client/src/components/Common/OverviewChart.jsx
+++ client/src/components/Common/OverviewChart.jsx
@@ -1,6 +1,50 @@
-import React from "react";
+import { useMemo } from "react";
+import { ResponsiveLine } from "@nivo/line";
+import { useTheme } from "@mui/material";
 
-const OverviewChart = () => {
+import { useGetSalesQuery } from "../../services/api/api";
+
+const OverviewChart = ({ isDashboard = false, view }) => {
+  const { data, isLoading } = useGetSalesQuery();
+  console.log("🚀 ~ file: OverviewChart.jsx:9 ~ OverviewChart ~ data", data);
+
+  const theme = useTheme();
+
+  // To make sure that this value doesn't change every time the component re-renders
+  const [totalSalesLine, totalUnitsLine] = useMemo(() => {
+    if (!data) return [];
+
+    const { overallStat } = data;
+    const { dailyData, monthlyData } = overallStat[0];
+
+    const totalSalesLine = {
+      id: "totalSales",
+      color: theme.palette.primary[600],
+      data: [],
+    };
+
+    Object.values(monthlyData).reduce(
+      (acc, { month, totalSales, totalUnits }) => {
+        const curSales = acc.sales + totalSales;
+        const curUnits = acc.units + totalUnits;
+
+        totalSalesLine.data = [
+          ...totalSalesLine.data,
+          { x: month, y: curSales },
+        ];
+        totalUnitsLine.data = [
+          ...totalUnitsLine.data,
+          { x: month, y: curUnits },
+        ];
+
+        return { sales: curSales, units: curUnits };
+      },
+      { sales: 0, units: 0 }
+    );
+
+    return [[totalSalesLine], [totalUnitsLine]];
+  }, [data]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return <div>OverviewChart</div>;
 };
 
