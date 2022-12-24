7122038bcd17d2a373813d49c7993a6490dbbbb5
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-24 20:27:55 +0530

    Added the dashboard controller to get the stats for the dashboard, have transactions, affiliate stats, monthly, dialy sales stats and many more

--- server/routes/general.route.js
+++ server/routes/general.route.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const { getUser } = require("../controllers/general.controller");
+const {
+  getUser,
+  getDashboardStats,
+} = require("../controllers/general.controller");
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
+router.get("/dashboard", getDashboardStats);
 
 module.exports = router;
