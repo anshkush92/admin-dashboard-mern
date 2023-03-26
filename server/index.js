80a8d08c009c83efbcfd53f6e6f1c4513b7121a9
anshkush92college <imt_2020013@iiitm.ac.in>
2023-03-26 21:25:47 +0530

    Dockerized the appliication git add . yay working correctly
    Signed-off-by: anshkush92college <imt_2020013@iiitm.ac.in>

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
 /* IMPORTS */
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 // For parsing the body of the request
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 // For interacting with the mongoDb
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 // For allowing cross origin requests
-const cors = require("cors");
+const cors = require('cors');
 // For securing the app by setting up various headers
-const helmet = require("helmet");
+const helmet = require('helmet');
 // For logging the requests
-const morgan = require("morgan");
+const morgan = require('morgan');
 
-const clientRoutes = require("./routes/client.route.js");
-const generalRoutes = require("./routes/general.route.js");
-const managementRoutes = require("./routes/management.route.js");
-const salesRoutes = require("./routes/sales.route.js");
+const clientRoutes = require('./routes/client.route.js');
+const generalRoutes = require('./routes/general.route.js');
+const managementRoutes = require('./routes/management.route.js');
+const salesRoutes = require('./routes/sales.route.js');
 
 /* CONFIGURATION */
 const app = express();
@@ -24,23 +24,23 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 // Allows us to make the cross origin sharing requests
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 // Logs the requests
-app.use(morgan("common"));
+app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use("/client", clientRoutes);
-app.use("/general", generalRoutes);
-app.use("/management", managementRoutes);
-app.use("/sales", salesRoutes);
+app.use('/client', clientRoutes);
+app.use('/general', generalRoutes);
+app.use('/management', managementRoutes);
+app.use('/sales', salesRoutes);
 
-app.get("/", async (req, res, next) => {
+app.get('/', async (req, res, next) => {
   try {
     res.status(200).json({
-      message: "Welcome to the MOCK DATA API for the ADMIN DASHBOARD",
+      message: 'Welcome to the MOCK DATA API for the ADMIN DASHBOARD',
     });
   } catch (error) {
     res.status(404).json({ message: error.message });
@@ -52,14 +52,14 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
 /* MONGODB SETUP */
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Successfully connected to the database");
+    console.log('Successfully connected to the database');
   })
   .catch((err) => {
     console.log(err);
